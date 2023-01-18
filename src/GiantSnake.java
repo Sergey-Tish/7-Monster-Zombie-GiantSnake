7e1d295885b1e80fafa1a429f79dcfbf55a1ef6f
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-18 23:42:31 +0700

    first commit, and mb end?

---
+++ src/GiantSnake.java
@@ -0,0 +1,18 @@
+public class GiantSnake extends Monster {
+    public final static String scream = "Ssssss";
+    //Fix this class
+    public GiantSnake(String name) {
+        super(name + " the Zombie", 5);
+    }
+
+    public void growl(){
+        System.out.println(scream);
+    }
+
+    @Override
+    public void attack(Monster monster) {
+        growl();
+    }
+
+
+}
---
+++ src/Zombie.java
@@ -0,0 +1,19 @@
+public class Zombie extends Monster {
+    public final static String scream = "Raaaauuughhhh";
+    //Fix this class
+    public Zombie(String name) {
+        super(name + " the Zombie", 5);
+    }
+
+    public void growl(){
+        System.out.println(scream);
+    }
+
+    @Override
+    public void attack(Monster monster){
+        monster.damage(getForce());
+        growl();
+    }
+
+
+}
